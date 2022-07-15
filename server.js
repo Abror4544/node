c2a305732677a99375a1c2afa17da4af25014bc1
Abror4544 <corporationsystems7@gmail.com>
2022-07-15 19:03:48 +0500

    Connect express

--- server.js
+++ server.js
@@ -1,15 +1,9 @@
-const http = require("http");
+const express = require('express');
 
-const hostname = "127.0.0.1";
+const app = express();
 
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello, World! \n");
+app.get('/', (req, res) => {
+  res.send('Hi!');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}`);
-});
+app.listen(3000, () => console.log('Server ready'));
