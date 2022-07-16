c234d5d13af0c0b41e3f6bdf233a7b9d0c8f01e7
Abror4544 <corporationsystems7@gmail.com>
2022-07-16 14:58:21 +0500

    Colorly outputs

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require("express");
+const chalk = require("chalk");
 
 require("dotenv").config();
 
 const app = express();
 
-console.time('doSomething()');
 app.get("/", (req, res) => {
   res.send("Hi!");
 });
-console.timeEnd('doSomething()');
 
-const server = app.listen(3000, () => console.log("Server is ready!"));
+console.log("\x1b[33m%s\x1b[0m", "hi!");
+console.log(chalk.greenBright("hi!"));
+
+const server = app.listen(3000, () =>
+  console.log(chalk.greenBright("Server is ready!"))
+);
 
 process.on("SIGTERM", () => {
   server.close(() => {
