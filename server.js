9b41b166299ffb54ff5018b9f08c078b4640f499
Abror4544 <corporationsystems7@gmail.com>
2022-07-16 14:56:45 +0500

    Time counter

--- server.js
+++ server.js
@@ -4,14 +4,11 @@ require("dotenv").config();
 
 const app = express();
 
+console.time('doSomething()');
 app.get("/", (req, res) => {
   res.send("Hi!");
 });
-
-const oranges = ["orange", "orange"];
-oranges.forEach((fruit) => {
-  console.count(fruit);
-});
+console.timeEnd('doSomething()');
 
 const server = app.listen(3000, () => console.log("Server is ready!"));
 
