8de952472682c6b472051e0ffaa9846bca3cfb00
Abror4544 <corporationsystems7@gmail.com>
2022-07-16 16:02:44 +0500

    Event loop queue

--- server.js
+++ server.js
@@ -12,16 +12,6 @@ app.get("/", (req, res) => {
 
 const bar = new ProgressBar(":bar", { total: 100 });
 
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-readline.question(`What's your name?`, name => {
-  console.log(`Hi ${name}!`);
-  readline.close();
-});
-
 const timer = setInterval(async () => {
   bar.tick();
   if (bar.complete) {
