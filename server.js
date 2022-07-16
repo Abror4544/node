862b0af594c1cc89e0a165cb63c3ead30fb72d55
Abror4544 <corporationsystems7@gmail.com>
2022-07-16 17:07:59 +0500

    fs stats

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const chalk = require("chalk");
 const ProgressBar = require("progress");
 const EventEmitter = require("events");
+const fs = require("fs");
 
 require("dotenv").config();
 
@@ -31,6 +32,18 @@ const timer = setInterval(async () => {
     );
   }
 }, 1);
+
+fs.stat("./queue.js", (err, stats) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  console.log(stats.isFile()); // true
+  console.log(stats.isDirectory()); // false
+  console.log(stats.isSymbolicLink()); // false
+  console.log(stats.size); // 1024000 //= 1MB
+});
 
 process.on("SIGTERM", () => {
   server.close(() => {
