7b4b2d6bc6f964293a10feb6d6efed2815ec1920
Abror4544 <corporationsystems7@gmail.com>
2022-07-16 14:04:21 +0500

    Express

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hi!');
+app.get("/", (req, res) => {
+  res.send("Hi!");
 });
 
-app.listen(3000, () => console.log('Server ready'));
+const server = app.listen(3000, () => console.log("Server ready"));
+
+process.on("SIGTERM", () => {
+  server.close(() => {
+    console.log("Process terminated");
+  });
+});
