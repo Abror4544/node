e0bbeff379a8dfba4cf71c0a9f77cc4bae03b156
Abror4544 <corporationsystems7@gmail.com>
2022-07-16 14:50:13 +0500

    Variables with arguments

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require("express");
-//const repl = require("repl");
 
 require("dotenv").config();
-
-//repl.start();
 
 const app = express();
 
@@ -11,7 +8,9 @@ app.get("/", (req, res) => {
   res.send("Hi!");
 });
 
-console.log(process.argv);
+console.log("My %s has %d ears", "cat", 2);
+console.log("I am %i years old", "21.4");
+console.log("%o", console);
 
 const server = app.listen(3000, () => console.log("Server is ready!"));
 
