f04c8eeb786a9d62673b092cc20f0037ce725d45
Abror4544 <corporationsystems7@gmail.com>
2022-07-18 11:08:44 +0500

    ES6 module | hoist imports

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 //const express = require("express"); // CommonJS
 //const { hiCall, hiRole } = require("./queue"); // CommonJS
 
-import express from "express"; //   ES6 module
-import { hiCall, hiRole } from "./queue-es6.js"; // ES6 module
-
 const app = express();
 
 app.get("/", (req, res) => {
@@ -14,3 +11,6 @@ console.log(hiCall()); // ES6 module
 console.log(hiRole()); // ES6 module
 
 const server = app.listen(3000, () => console.log("Server is ready!"));
+
+import express from "express"; //   ES6 module
+import { hiCall, hiRole } from "./queue-es6.js"; // ES6 module
