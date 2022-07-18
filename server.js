579dd57670f8592eaea2e7bfdac83667fddb72c1
Abror4544 <corporationsystems7@gmail.com>
2022-07-18 10:44:28 +0500

    CommonJS | Desctructurisation

--- server.js
+++ server.js
@@ -1,11 +1,5 @@
 const express = require("express"); // CommonJS
-
-if (1 === 1) {
-  const foo = require("./queue"); // CommonJS
-
-  console.log(foo.hiMan()); // CommonJS
-  console.log(foo.hiGirl()); // CommonJS
-}
+const { hiMan, hiGirl } = require("./queue"); // CommonJS
 
 // import express from "express"; -   ES6 module
 // import hiMan from "./queue.js"; - ES6 module
@@ -15,5 +9,8 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Hi!");
 });
+
+console.log(hiMan()); // CommonJS
+console.log(hiGirl()); // CommonJS
 
 const server = app.listen(3000, () => console.log("Server is ready!"));
